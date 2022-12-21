f6ed7cca52df63edd3cd433bd608ba857b855be6
javierjazmin <javierjazmin@gmail.com>
2022-12-21 18:53:22 -0300

    primer proyecto de react

---
+++ src/components/MainContent.js
@@ -0,0 +1,53 @@
+import React, {Component} from "react";
+
+class MainContent extends Component(){
+
+  render(){
+  
+   
+    return (
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card h-100">
+              <img className="card-img-top" src="" alt=""/>
+              <div className="card-body">
+                <h4 className="card-title">titulo</h4>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card h-100">
+            <img src="" alt=""></img>
+              <div className="card-body">
+                <h4 className="card-title">titulo</h4>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card h-100">
+              <img className="card-img-top" src="" alt=""/>
+              <div className="card-body">
+                <h4 className="card-title">titulo</h4>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-lg-3 col-md-6 mb-4">
+            <div className="card h-100">
+              <img className="card-img-top" src="" alt=""/>
+              <div className="card-body">
+                <h4 className="card-title">titulo</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  }
+
+
+export default MainContent;
